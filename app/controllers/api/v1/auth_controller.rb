496fd6ea2fad0616f23693157a8baf8876f82f42
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-12-30 17:28:46 -0500

    fixed some params issues

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -6,10 +6,10 @@ class Api::V1::AuthController < ApplicationController
         #User#authenticate comes from BCrypt
         if @user && @user.authenticate(user_login_params[:password])
         # encode token comes from ApplicationController
-        token = encode_token({ user_id: @user.id })
-        render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+            token = encode_token({ user_id: @user.id })
+            render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
         else
-        render json: { message: 'Invalid username or password' }, status: :unauthorized
+            render json: { message: 'Invalid username or password' }, status: :unauthorized
         end
     end
     
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::UsersController < ApplicationController
      
       private
     def user_params
-        params.require(:user).permit(:username, :password, :agent, :admin, :avatar)
+        params.require(:user).permit(:username, :password, :password_confirmation, :agent, :admin, :avatar)
     end
     
 end
