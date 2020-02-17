141b9758650b3bc49743512d76666dab8640ff31
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2020-02-17 17:49:36 -0500

    changed origins for actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://backsupport-api.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['https://backsupport-api.herokuapp.com', 'http://backsupport-api.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://backsupport-client.herokuapp.com', 'http://backsupport-client.herokuapp.com']
 
 
   # config.web_socket_server_url = "wss:///cable" 
